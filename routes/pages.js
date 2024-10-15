47eeaab44e24ed80b8fd4dfc0626360133cfb89a
lior <lior610.aharon@gmail.com>
2024-10-16 00:57:44 +0300

    pages without comments

--- routes/pages.js
+++ routes/pages.js
@@ -7,19 +7,19 @@ router.get("/personal_area.html", loginController.isLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'personal_area.html'));
 });
 
-// router.get("/admin.html", loginController.isAdmin, (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public', 'admin.html'));
-// });
+router.get("/admin.html", loginController.isAdmin, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'admin.html'));
+});
 
 router.get("/edit_:page.html", loginController.isAdmin, (req, res) => {
     const page = req.params.page;
     res.sendFile(path.join(__dirname, '../public', `edit_${page}.html`));
 });
 
-// router.get("/admin/:page.html", loginController.isAdmin, (req, res) => {
-//     const page = req.params.page;
-//     res.sendFile(path.join(__dirname, '../public/admin', `${page}.html`));
-// });
+router.get("/admin/:page.html", loginController.isAdmin, (req, res) => {
+    const page = req.params.page;
+    res.sendFile(path.join(__dirname, '../public/admin', `${page}.html`));
+});
 
 
 module.exports = router;
